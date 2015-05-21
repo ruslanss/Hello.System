e8ba21fa3516a199a3a2f59c754561669e37127a
Mark.Gray <mark.gray@magsolutionslimited.co.uk>
2015-05-21 11:19:09 +0100

    Trying to make the build script work

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 cls
+".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+"packages\FAKE\tools\Fake.exe" build.fsx
 "packages\Paket.1.4.18\tools\paket" "install"
 "packages\FAKE\tools\Fake.exe" build.fsx
-pause
