c83b509c1622b9c0b2871a2127a786d34dd6c8d1
Mark.Gray <mark.gray@magsolutionslimited.co.uk>
2015-05-21 13:34:23 +0100

    Adding some verbosity to the build script output (simple @echo messages is all)

--- build.bat
+++ build.bat
@@ -1,5 +1,10 @@
 @echo off
 cls
+@echo INSTALLED PACKAGES:
+".paket\paket" show-installed-packages
+@echo CHECKING FOR OUTDATED PACKAGES:
 ".paket\paket" outdated
+@echo STARTING UPDATE
 ".paket\paket" update
+@echo BUILDING PROJECT
 "packages\FAKE\tools\Fake.exe" build.fsx
